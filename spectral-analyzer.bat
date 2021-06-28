b638956c68205ea1d20fc43d07c0608e5757e810
kei_e <k.eguchi84@gmail.com>
2021-06-28 14:56:53 +0900

    開発環境をdockerへ移行

--- spectral-analyzer.bat
+++ spectral-analyzer.bat
@@ -1,4 +1,4 @@
 @echo off\
 set url="http://127.0.0.1:8000"
 start chrome %url%
-cmd /k "./myvenv/Scripts/activate & python manage.py runserver "
+cmd /k "myvenv\Scripts\activate & py -3.6 manage.py runserver "
